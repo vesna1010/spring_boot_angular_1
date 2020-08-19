e71cb78ac468bf5643d7114cb4c8f52fb5f99bb7
Vesna S <vesnasdoboj@gmail.com>
2020-08-20 00:26:14 +0200

    Update app-routing.module.ts

--- getbook/src/app/app-routing.module.ts
+++ getbook/src/app/app-routing.module.ts
@@ -37,8 +37,12 @@ const routes: Routes = [
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  imports: [
+    RouterModule.forRoot(routes)
+  ],
+  exports: [
+    RouterModule
+  ]
 })
 export class AppRoutingModule { }
 
